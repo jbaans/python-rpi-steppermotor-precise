7c1c7b6d2ee0486051c5e5323104ab9240380797
jbaans <42092978+jbaans@users.noreply.github.com>
2021-05-05 21:09:33 +0200

    Fix duty cycle logic

--- steppermotor_precise/DRV8825.py
+++ steppermotor_precise/DRV8825.py
@@ -16,10 +16,10 @@ from steppermotor_precise.BasicDriver import BasicDriver
 class DRV8825(BasicDriver):
     """
     This class extends the BasicDriver class to implement the DRV8825
-    driver chip.
-    It allows for setting microstep size, specifies WAIT_ENABLE time
-    and an appropriate DEFAULT_DUTYCYCLE and defines the allowed microstep
-    sizes.
+    driver chip. The DRV8825 datasheet specifies a maximum step frequency
+    of 250 kHz and minimum step LOW and HIGH duations of 1.9 microseconds.
+    The class allows for setting microstep size, specifies WAIT_ENABLE time
+    and defines the allowed microstep sizes.
     It provides the step() function which allows for making a step with
     the size of any multiple of the specified microstep size.
     """
@@ -32,7 +32,6 @@ class DRV8825(BasicDriver):
                 1/32:[1, 0, 1]}
 
     WAIT_ENABLE = 0.05       # sets seconds for DRV8825 to stabilize, 1 ms in datasheet
-    DEFAULT_DUTYCYCLE = 25   # 0..255
 
     # default settings:
     stepsize = 1
@@ -53,7 +52,6 @@ class DRV8825(BasicDriver):
         self.set_stepsize(stepsize or self.stepsize)
         if self.verbosity >= 1:
             print("Initial step size: " + str(self.stepsize))
-            print("Default duty cycle (actual = 0): " + str(self.DEFAULT_DUTYCYCLE))
 
 
     def set_stepsize(self, size):
@@ -72,7 +70,7 @@ class DRV8825(BasicDriver):
             print("Step size is set to " + str(size))
 
 
-    def enable(self, frequency=None, dutycycle=None, stepsize=None, direction=None):
+    def enable(self, frequency=None, stepsize=None, direction=None):
         """Sets step size. Then enables motor driver chip by calling parent function.
 
         Args:
@@ -84,10 +82,10 @@ class DRV8825(BasicDriver):
         if stepsize:
             self.set_stepsize(stepsize)
 
-        super().enable(frequency=frequency, dutycycle=dutycycle, direction=direction)
+        super().enable(frequency=frequency, direction=direction)
 
 
-    def step(self, steps, frequency=None, dutycycle=None, stepsize=None):
+    def step(self, steps, frequency=None, stepsize=None):
         """Make the specified amount of whole steps at the specified frequency, 
         with the specified duty cycle and in the specified direction, or their
         current values.
@@ -98,7 +96,6 @@ class DRV8825(BasicDriver):
         Args:
             steps (float)): number of whole steps to make.
             frequency (int, optional): PWM frequency (Hz). Defaults to None.
-            dutycycle (int, optional): PWM duty cycle 0..255 (for 0..100%). Defaults to None.
             stepsize (float, optional): Microstep size. Any of 1, 1/2, 1/4, 1/8, 1/16, 1/32
                                         or None. Defaults to None.
 
@@ -115,7 +112,6 @@ class DRV8825(BasicDriver):
 
         # can only make whole positive number of pulses
         pulses = int(abs(steps) / self.stepsize)
-        stepsmade = super().pulse(pulses, frequency=frequency, dutycycle=dutycycle,
-                                  direction=direction) * self.stepsize
+        stepsmade = super().pulse(pulses, frequency=frequency, direction=direction) * self.stepsize
         # return the amount of steps made, with the sign of the requested direction
         return math.copysign(stepsmade, steps)
